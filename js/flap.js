27947363862231d444997a67988250b48f6e7bd0
Andy Cross <andy@elastacloud.com>
2013-02-13 20:50:26 +0000

    Goose has new tabs, but they might not last ;-)

---
+++ js/flap.js
@@ -0,0 +1,47 @@
+function search()
+{
+	$('#prepareToShare').empty();
+    $('#playlistDiv').html("<div class='loading'><div class='throbber'><div></div></div></div>")
+
+    var search = new models.Search(document.getElementById('searchTerm').value);       
+    var playlist = new models.Playlist(); 
+    search.localResults = models.LOCALSEARCHRESULTS.APPEND;
+
+    search.observe(models.EVENT.CHANGE, function() {
+
+        search.tracks.forEach(function(track) {
+            playlist.add(track);
+        });
+
+        
+		var list = getCommonList(playlist);
+
+        list.node.classList.add('sp-light');
+
+        $('#playlistDiv').empty();
+        document.getElementById('playlistDiv').appendChild(list.node);
+    });
+
+    search.appendNext();                
+}
+
+function stage(trackUri)
+{
+	var t = models.Track.fromURI(trackUri, function(track) {
+			    $("#prepareToShare").html(tmpl("prepareToShare_tmpl", track));
+			});
+}
+
+function getCommonList(playlist)
+{
+	return new views.List(playlist, function(track) {
+                var trackEx = new views.Track(track,
+                                    views.Track.FIELD.NAME |
+                                    views.Track.FIELD.ARTIST |
+                                    views.Track.FIELD.DURATION);
+
+                $(trackEx.node).append("<span class='rbutton'><button class='sp-button sp-flat' onclick='stage(\"" + track.uri + "\")'>honk</button></span>");
+                return trackEx;
+                                            });
+
+}
